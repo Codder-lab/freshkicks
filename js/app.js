3ba41d5df9c21d3c60b8a65370c93894c1be95ab
suyashpotdar <suyashpotdar03@gmail.com>
2024-08-30 20:33:59 +0530

    Add payment modal

--- js/app.js
+++ js/app.js
@@ -87,6 +87,10 @@ const currentProductPrice = document.querySelector(".productPrice");
 const currentProductColors = document.querySelectorAll(".color");
 const currentProductSizes = document.querySelectorAll(".size");
 
+const productButton = document.querySelector(".productButton");
+const payment = document.querySelector(".payment");
+const close = document.querySelector(".close");
+
 menuItems.forEach((item, index) => {
     item.addEventListener("click", () => {
         // change the current slide
@@ -111,4 +115,23 @@ currentProductColors.forEach((color, index) => {
     color.addEventListener("click", () => {
         currentProductImg.src = choosenProduct.colors[index].img;
     });
+});
+
+currentProductSizes.forEach((size, index) => {
+    size.addEventListener("click", () => {
+        currentProductSizes.forEach((size) => {
+            size.style.backgroundColor = "white";
+            size.style.color = "black";
+        })
+        size.style.backgroundColor = "black";
+        size.style.color = "white";
+    });
+});
+
+productButton.addEventListener("click", () => {
+    payment.style.display = "flex";
+});
+
+close.addEventListener("click", () => {
+    payment.style.display = "none";
 });
